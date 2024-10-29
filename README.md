7a04a231a44092f52848b735e189f93e796d491c
Dean Nguyen <dnp2k4@gmail.com>
2024-10-28 22:15:23 -0700

    rough draft

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-###Going to be using this for the ICSSC Fellowship course
+# Dean's Personal Website
+
+Welcome to my personal website! This project showcases my projects and experience as an aspiring software engineer. There are also a few photos of myself! The website is built using HTML, CSS, and Bootstrap to ensure a responsive and user-friendly design.
+
+## Run
+Ensure that bootstrap is installed. Start by running index.html
+
+## Features
+- Personal introduction and experience section
+- Project showcase with descriptions, thumbnail, and links
+- Contact information and social media links
+- Responsive design using Bootstrap
+- Gallery displaying hobbies
+
+## Technologies Used
+- **HTML5**
+- **CSS**
+- **Bootstrap**
+- **JavaScript**
--- scripts/insertNav.js
+++ scripts/insertNav.js
@@ -1,7 +1,7 @@
 // Function to load the navbar and footer content
-function loadNavAndFooter() {
+function loadNav() {
     // Fetch and insert the navbar HTML
-    fetch("/components/navbar.html")
+    fetch("components/navbar.html")
       .then(response => response.text())
       .then(html => {
         document.getElementById("navbar-placeholder").innerHTML = html;
@@ -9,20 +9,10 @@ function loadNavAndFooter() {
       .catch(error => {
         console.error("Error loading navbar:", error);
       });
-  
-    // Fetch and insert the footer HTML
-    fetch("footer.html")
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById("footer-placeholder").innerHTML = html;
-      })
-      .catch(error => {
-        console.error("Error loading footer:", error);
-      });
   }
   
   // Call the function when the page loads
   window.onload = function() {
-    loadNavAndFooter();
+    loadNav();
   };
   
